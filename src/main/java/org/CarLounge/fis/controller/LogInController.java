f0f6a40eb3e8ce3f2b80579eadcb56bdffcb13d9
Radulz <mircea.matei@student.upt.ro>
2021-04-20 18:04:06 +0300

    Added transition between log in scene and register scene.

--- src/main/java/org/CarLounge/fis/controller/LogInController.java
+++ src/main/java/org/CarLounge/fis/controller/LogInController.java
@@ -10,20 +10,49 @@ import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 
 public class LogInController {
     @FXML
-    Button goToRegister;
+    private Button goToRegister;
+
+    @FXML
+    private Button logintoacc;
+
+    @FXML
+    private AnchorPane logInContainer;
+
+    @FXML
+    private StackPane mainContainer;
 
     public void switchToRegister(MouseEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
-        Stage window= (Stage)goToRegister.getScene().getWindow();
-        window.setScene(new Scene(root));
-    }
+        Scene scene = goToRegister.getScene();
+        root.translateXProperty().set(scene.getWidth());
 
-    @FXML
-    Button logintoacc;
+        mainContainer.getChildren().add(root);
+
+        Timeline timeline=new Timeline();
+        KeyValue KV=new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame KF = new KeyFrame(Duration.seconds(0.6), KV);
+        timeline.getKeyFrames().add(KF);
+        timeline.setOnFinished(t -> { mainContainer.getChildren().remove(logInContainer); });
+        timeline.play();
+
+        //Stage window= (Stage)goToRegister.getScene().getWindow();
+        //window.setScene(new Scene(root));
+    }
 
     public void switchToHomeScene(MouseEvent event) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("home.fxml"));
