22c76b77993f9e5a9618cd1f0827c61a1e818f7c
Pepperolls <rares.mladin@gmail.com>
2021-05-12 03:05:22 +0300

    CLG-57, CLG-58, CLG-59 Created the form which contains the car`s details when trying to list it, along with the cancel button which gets you back to the home scene.

--- src/main/java/org/CarLounge/fis/controller/NewListing.java
+++ src/main/java/org/CarLounge/fis/controller/NewListing.java
@@ -1,4 +1,36 @@
 package org.CarLounge.fis.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewListing {
+    @FXML
+    private JFXButton switchToHome;
+
+    @FXML
+    private StackPane contentArea;
+
+    public void cancel(ActionEvent actionEvent) {
+    }
+
+    public void ok(ActionEvent actionEvent) {
+    }
+
+    public void addCar(MouseEvent mouseEvent) {
+    }
+
+    public void home(MouseEvent event) throws Exception{
+        Parent fxml = FXMLLoader.load(getClass().getClassLoader().getResource("home.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
 }
