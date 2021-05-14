6b6681ecd4b84e9997143ebdcda6e349109f7786
Radulz <mircea.matei@student.upt.ro>
2021-05-14 23:04:23 +0300

    CLG-91 Verified the entered email, if it exists and if it is valid.

--- src/main/java/org/CarLounge/fis/controller/ForgotPasswordController.java
+++ src/main/java/org/CarLounge/fis/controller/ForgotPasswordController.java
@@ -6,9 +6,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.CarLounge.fis.exceptions.AccountDoesNotExist;
+import org.CarLounge.fis.exceptions.EmailFieldIsEmpty;
+import org.CarLounge.fis.exceptions.TextIsNotAValidEmail;
+import org.CarLounge.fis.model.Client;
+import org.CarLounge.fis.model.Provider;
+import org.CarLounge.fis.services.ClientService;
+import org.CarLounge.fis.services.ProviderService;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,10 +27,16 @@ public class ForgotPasswordController implements Initializable {
     @FXML
     public ImageView exit;
     @FXML
+    public TextField email;
+    @FXML
+    public Text forgotMessage;
+    @FXML
     private JFXButton cancel;
     @FXML
     private JFXButton next;
 
+    private static String username;
+    private static String type;
 
     public void initialize(URL location, ResourceBundle resources) {
         exit.setOnMouseClicked(event -> {
@@ -35,15 +50,69 @@ public class ForgotPasswordController implements Initializable {
         window.setScene(new Scene(root));
     }
 
-    public void switchToNext(MouseEvent mouseEvent) throws IOException {
-        //CheckCNP.fxml if individual
-        /*Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("CheckCNP.fxml"));
-        Stage window = (Stage)next.getScene().getWindow();
-        window.setScene(new Scene(root));*/
+    private static boolean checkEmail(String email){
+        String [] acceptedMails = {"@yahoo.com", "@yahoo.co.uk", "@yahoo.ro", "@gmail.com", "@student.upt.ro", "@cs.upt.ro"};
+        for(String sir : acceptedMails){
+            if(email.endsWith(sir))
+                return true;
+        }
+        return false;
+    }
 
-        //CheckTaxRegNo.fxml if legal person
-        /*Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("CheckTaxRegNo.fxml"));
-        Stage window = (Stage)next.getScene().getWindow();
-        window.setScene(new Scene(root));*/
+    private static boolean checkExistence(String user) {
+
+        type = "";
+        for(Client client: ClientService.getClientRepository().find()) {
+            if (user.equals(client.getEmail())) {
+                    type = "Individual";
+            }
+        }
+        for(Provider provider: ProviderService.getProviderRepository().find()){
+            if(user.equals(provider.getEmail()) && provider.getFirstName().equals("LegalPerson")){
+                type = "Legal";
+            }
+            else if(user.equals(provider.getEmail()) && !provider.getFirstName().equals("LegalPerson")){
+                type = "Individual";
+            }
+        }
+        if(type == "") {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+
+    public static void checkEmailField(String email) throws EmailFieldIsEmpty, TextIsNotAValidEmail, AccountDoesNotExist {
+        if(email == ""){
+            throw  new EmailFieldIsEmpty();
+        }
+        else if(!checkEmail(email)){
+            throw new TextIsNotAValidEmail();
+        }
+        else if(!checkExistence(email)){
+            throw new AccountDoesNotExist();
+        }
+    }
+
+    public void switchToNext(MouseEvent mouseEvent) throws IOException {
+        username = email.getText();
+        try{
+            checkEmailField(username);
+            if(type.equals("Individual")){
+                Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("CheckCNP.fxml"));
+                Stage window = (Stage)next.getScene().getWindow();
+                window.setScene(new Scene(root));
+            }
+            else if(type.equals("Legal")){
+                Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("CheckTaxRegNo.fxml"));
+                Stage window = (Stage)next.getScene().getWindow();
+                window.setScene(new Scene(root));
+            }
+        }
+        catch(EmailFieldIsEmpty | TextIsNotAValidEmail | AccountDoesNotExist e){
+            forgotMessage.setText(e.getMessage());
+        }
     }
 }
